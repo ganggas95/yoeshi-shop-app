28b45a71bfc647947d7980ee637600da1257ff4a
ganggas <subhannizar25@gmail.com>
2024-01-21 08:16:46 +0800

    CU-860t728j5 - As a front-end engineer, I will be able to setup colors theme in tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,12 +7,18 @@ const config: Config = {
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      'primary': '#40BFFF',
+      'secondary': '#FF4858',
+      'tertiary': '#BCDDFE',
+      'neutral': '#373737'
+    },
+    fontFamily: {
+      poppins: ['Poppins', 'sans-serif'],
+      raleway: ['Raleway', 'serif'],
+      inter: ['Inter', 'sans-serif'],
+    },
     extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
     },
   },
   plugins: [],
